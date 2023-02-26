92236bbfb5a7fccdca27644abeabb4c81cc73988
Ezequiel <ezequielroca99@gmail.com>
2023-02-26 03:22:39 -0300

    modificacion 2

--- EJEMPLO GIT/Form1.cs
+++ EJEMPLO GIT/Form1.cs
@@ -16,5 +16,10 @@ namespace EJEMPLO_GIT
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
